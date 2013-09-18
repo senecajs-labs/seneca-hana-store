f7a92b11d178450df907d37c51774a5c4504b114
Marian Radulescu <marian.radulescu@gmail.com>
2013-09-19 02:16:03 +0300

    Changed code to retrieve the table metadata at the first request

--- tests/hana.test.js
+++ tests/hana.test.js
@@ -16,14 +16,41 @@ var options = {
     idtype: 'sequence'
 }
 
-var si = seneca({log:'print'})
+var si = seneca()
 si.use(require('..'), options)
 
 si.__testcount = 0
 var testcount = 0
 
+var cleanup_cmds = [
+  'DELETE FROM "foo"',
+  'DELETE FROM "moon_bar"'
+]
+
+function prepdb(dbi, cmds, cb) {
+  function cmdit(query, callback) {
+    console.log(query)
+    dbi.query('\"' + options.schema + '\"', query, function (err, res) {
+      console.log(res)
+      callback(err, res)
+    })
+  }
+  async.eachSeries(cmds, cmdit, cb)
+}
+
 
 describe('hana', function () {
+
+  before(function (done) {
+    si.ready(function () {
+      var ent = si.make$('sys')
+      ent.native$(function (err, dbi) {
+        prepdb(dbi, cleanup_cmds, done)
+      })
+    })
+  })
+
+
   it('basic', function (done) {
     testcount++
     shared.basictest(si, done)
